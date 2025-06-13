cea1f228884b7d4753c00cfb320d93bb9dc22946
Shubham Dhapola <shubhamdhapola143@gmail.com>
2025-06-13 21:10:21 +0530

    Logout issue fixed

--- backend/utils/sendCookie.js
+++ backend/utils/sendCookie.js
@@ -1,8 +1,8 @@
 export const sendCookie = (token, res) => {
-    res.cookie('token',token,{
-        secure : true,
-        maxAge : 7 * 24 * 60 * 60 * 1000,
-        httpOnly : true,
-        sameSite : 'None' 
+    res.cookie('token', token, {
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax'
     })
 }
